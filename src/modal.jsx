924154998639ffdf06d9a06ee1f737623dcaa65d
QuinnTracy1129 <92907201+QuinnTracy1129@users.noreply.github.com>
2023-04-02 19:51:36 +0800

    Update modal.jsx

--- src/modal.jsx
+++ src/modal.jsx
@@ -85,6 +85,7 @@ export default function MarkdownModal({
 
     navigator.clipboard.writeText(table);
     toast.success("Markdown Copied to Clipboard.");
+    toast.info("Remember to double check the output before saving it.");
   };
 
   return (
